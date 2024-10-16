b8922b8f48a65478fe2c49c40f95fb20843456d2
Harry Xiong <harryxiong24@gmail.com>
2024-10-15 21:19:41 -0700

    feat: optimize code

--- echo-version/services/event.go
+++ echo-version/services/event.go
@@ -12,14 +12,12 @@ import (
 func GetEvent(c echo.Context) error {
 	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
 	}
 
 	event, err := models.GetEventById(eventId)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch event"})
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch event"})
 	}
 
 	return c.JSON(http.StatusOK, event)
@@ -28,8 +26,7 @@ func GetEvent(c echo.Context) error {
 func GetEvents(c echo.Context) error {
 	events, err := models.GetAllEvents()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
 	}
 
 	return c.JSON(http.StatusOK, events)
@@ -57,37 +54,32 @@ func CreateEvent(c echo.Context) error {
 func UpdateEvent(c echo.Context) error {
 	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
 	}
 
-	userId := c.Get("userId").(int)
-	event, err := models.GetEventById(eventId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch event"})
-		return err
-	}
+	// userId := c.Get("userId").(int)
+	// event, err := models.GetEventById(eventId)
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch event"})
+	// }
 
 	// Check if the user is the owner of the event
-	if event.UserID != int64(userId) {
-		c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorize to update event"})
-		return err
-	}
+	// if event.UserID != int64(userId) {
+	// 	return c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorize to update event"})
+	// }
 
 	var updateEvent models.Event
 	err = c.Bind(&updateEvent) // This will bind the request body to the event struct
 	if err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string{"error: cannot parse data": err.Error()})
-		return err
+		return c.JSON(http.StatusBadRequest, map[string]string{"error: cannot parse data": err.Error()})
 	}
 
 	updateEvent.ID = eventId
 
 	err = updateEvent.Update()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not update event"})
 		fmt.Println(err)
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not update event"})
 	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{"status": "event updated", "event": updateEvent})
@@ -97,27 +89,24 @@ func DeleteEvent(c echo.Context) error {
 	// Code for deleting an event
 	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch events"})
 	}
 
-	userId := c.Get("userId").(int)
+	// userId := c.Get("userId").(int)
 	event, err := models.GetEventById(eventId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not fetch event"})
 		return err
 	}
 
-	if event.UserID != int64(userId) {
-		c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorize to delete event"})
-		return err
-	}
+	// if event.UserID != int64(userId) {
+	// 	return c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorize to delete event"})
+	// }
 
 	err = event.Delete()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not delete event"})
 		fmt.Println(err)
-		return err
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not delete event"})
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"status": "event deleted"})
